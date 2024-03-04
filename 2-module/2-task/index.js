d7bd3c7582ff0beaa6f1233be6a2d4663b351ca5
moiseyka <moiseyka86@mail.ru>
2024-03-04 10:27:58 +0300

    пустой ли объект

---
+++ 2-module/2-task/index.js
@@ -0,0 +1,11 @@
+let obj ={
+  name: 'Иван',
+  fam: 'Иванов',
+  age:'23',  
+}
+function isEmpty(obj) {
+ let massiv= Object.keys(obj)
+ if (massiv.length > 0) return true
+  else return false;
+}
+isEmpty(obj);
